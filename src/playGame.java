497d5588f7eaccc5962561943e457f06f1f7b893
oakley8sam <oakley8sam@aim.com>
2015-02-22 16:06:54 -0500

    Update playGame.java
    updated play game

--- src/playGame.java
+++ src/playGame.java
@@ -5,25 +5,30 @@ import java.util.Collection;
 
 public class playGame {
 	public static void main (String [] args){
-		Stack<Integer> draw=new Stack<Integer>();
-		Stack<Integer> discard= new Stack<Integer>();
 		int numPlay;
 		Game g1;
 		g1= new Game();
-		//g1.newGame();
-		Scanner s=new Scanner(System.in);
-		System.out.println("How many players are there?");
+		g1.newGame();
+		/*Scanner s=new Scanner(System.in);
+		System.out.println("How many players are there? (2-4)");
 		numPlay=s.nextInt();
-		Player p1= new Player(null, null, 0);
-		Player p2= new Player(null, null, 0);
+		Player p1= new Player();
+		Player p2= new Player();
 		if (numPlay>=3){
-			Player p3= new Player(null,null,0);
+			Player p3= new Player();
 		}
 		if (numPlay==4){
-			Player p4= new Player(null, null, 0);
-		}
-		g1.newGame(numPlay);
+			Player p4= new Player();
+		}*/
+		//g1.newGame(numPlay);
+		System.out.println(g1.getDraw());
 		g1.newHand();
+		
+		System.out.println(g1.getPlayer1().getRack());
+		System.out.println(g1.getPlayer2().getRack());
+		System.out.println(g1.getDraw());
+		g1.getPlayer1().drawDraw();
+		System.out.println(g1.getPlayer1().getHand());
 		//starts the game by asking for player number
 		//deals cards for new hand
 		//asks each player where the want to draw from
